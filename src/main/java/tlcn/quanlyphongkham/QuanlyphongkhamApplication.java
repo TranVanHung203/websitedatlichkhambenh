0dedea0cd0ea42e71181edb6aff5dae7f94a17f8
Tran Van Hung <hungtranvan1110@gmail.com>
2024-10-16 09:33:46 +0700

    Add all files

---
+++ src/main/java/tlcn/quanlyphongkham/QuanlyphongkhamApplication.java
@@ -0,0 +1,13 @@
+package tlcn.quanlyphongkham;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanlyphongkhamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanlyphongkhamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tlcn/quanlyphongkham/QuanlyphongkhamApplicationTests.java
@@ -0,0 +1,13 @@
+package tlcn.quanlyphongkham;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanlyphongkhamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
